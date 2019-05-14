7d703653a28134bddcb18338439e1232154cbc17
Abagail Hudson <abagailanne@gmail.com>
2019-05-14 23:53:17 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -26,8 +26,8 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-holiday_hash[:winter][:christmas] << supply
-holiday_hash[:winter][:new_years] << supply
+  holiday_hash[:winter][:christmas] << supply
+  holiday_hash[:winter][:new_years] << supply
 end
 
 
@@ -47,7 +47,7 @@ end
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
 
-holiday_hash[:winter].values.flatten
+  holiday_hash[:winter].values.flatten
 
 
 end
@@ -74,14 +74,14 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   new_array = []
-  holiday_hash.each do |season, _holiday_hash|
-   _holiday_hash.each do |holiday_name, supplies_array|
-     supplies_array.each do |supply|
-       if supply == "BBQ"
-         new_array << holiday_name
-       end
-     end
-   end
- end
- new_array
+    holiday_hash.each do |season, _holiday_hash|
+      _holiday_hash.each do |holiday_name, supplies_array|
+        supplies_array.each do |supply|
+          if supply == "BBQ"
+            new_array << holiday_name
+          end
+        end
+      end
+    end
+    new_array
 end
